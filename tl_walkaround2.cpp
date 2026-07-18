2aa1a7ea2a9a7486785a8b319accb87c09bafd7d
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2026-07-18 16:07:42 +0900

    v1.90

--- tl_walkaround2.cpp
+++ tl_walkaround2.cpp
@@ -42,10 +42,10 @@ namespace logging = AviUtl2::logging;
 // plugin info.
 ////////////////////////////////
 #define PLUGIN_NAME		L"TLショトカ移動2"
-#define PLUGIN_VERSION	"v1.82-wip"
+#define PLUGIN_VERSION	"v1.90"
 #define PLUGIN_AUTHOR	L"σ軸"
-#define LEAST_AVIUTL2_VER_STR	"version 2.0.54"
-constexpr uint32_t least_aviutl2_ver_num = 2005400;
+#define LEAST_AVIUTL2_VER_STR	"version 2.1.1"
+constexpr uint32_t least_aviutl2_ver_num = 2010100;
 
 
 ////////////////////////////////
